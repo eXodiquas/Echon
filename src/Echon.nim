84f17743f0b57e52454e042fd3e54953d22df72f
Timo 'eXodiquas' Netzer <exodiquas@gmail.com>
2020-07-12 02:33:20 +0200

    refactoring and documentation comments

--- src/Echon.nim
+++ src/Echon.nim
@@ -1,17 +1,24 @@
-# This is just an example to get you started. A typical library package
-# exports the main API in this file. Note that you cannot rename this file
-# but you can remove it if you wish.
-
 import sequtils
 
 type
+  ## A tuple that represents a replacement rule for the l-system.
   Prule* = tuple
     sym: char
     word: string
 
+
+
 proc generateSystem*(start: string, p: seq[Prule], cycle: int): string =
+  ## Takes a starting word, a seq of rules and a int for the number of recurring cycles 
+  ## and produces a string that represents the input string after the l-system transformations.
+
   var res = ""
   
+  #[
+    Checks if there is a rule that applies to the current char c in the current word. 
+    If so apply the first rule that matches the character. 
+    If there is no such rule just write the character as it is.
+  ]#
   for c in start:
     let interm = filter(p, proc (x: Prule): bool = x[0] == c)
     if interm.len > 0:
@@ -19,6 +26,10 @@ proc generateSystem*(start: string, p: seq[Prule], cycle: int): string =
     else:
       res.add(c)
   
+  #[
+    Checks if we are at the right recursion depth, if so return the final result.
+    If not recur with the new result as input string, the same rules and cycle reduced by 1.
+  ]#
   if cycle == 0:
     res
   else:
